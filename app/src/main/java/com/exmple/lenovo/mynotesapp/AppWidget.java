5445109e80a9ee92da6cb8d6cd7dc19fc7ab12a9
IlmuKomputerUnila <tomblokk.id@gmail.com>
2019-06-17 22:18:49 +0700

    nav

--- app/src/main/java/com/exmple/lenovo/mynotesapp/AppWidget.java
+++ app/src/main/java/com/exmple/lenovo/mynotesapp/AppWidget.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.widget.RemoteViews;
 
 import com.exmple.lenovo.mynotesapp.view.MainActivity;
+import com.exmple.lenovo.mynotesapp.view.NewBerita;
 
 
 public class AppWidget extends AppWidgetProvider {
@@ -20,6 +21,24 @@ public class AppWidget extends AppWidgetProvider {
 
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
             views.setOnClickPendingIntent(R.id.btnimg, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appwidgetid,views);
+        }
+        for (int appwidgetid : appWidgetIds){
+            Intent intent = new Intent(context, NewBerita.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
+            views.setOnClickPendingIntent(R.id.btnberita, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appwidgetid,views);
+        }
+        for (int appwidgetid : appWidgetIds){
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
+            views.setOnClickPendingIntent(R.id.btnnewnote, pendingIntent);
 
             appWidgetManager.updateAppWidget(appwidgetid,views);
         }
